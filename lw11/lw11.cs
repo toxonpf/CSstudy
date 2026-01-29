005d96271b913fe1b24a344a322c018b1eb176cd
toxon <toxonpf@gmail.com>
2026-01-29 12:39:13 +0500

    Добавлен проект lw11: консольное приложение для задач
    В решение добавлен новый проект lw11 на .NET 10.0. Реализовано консольное приложение для создания файла и ввода задач с сохранением в файл. В toxonpf.txt добавлены тестовые строки.

---
+++ lw11/lw11.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace lw11
+{
+    public class lw11
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Users\toxon\source\repos\toxonpf\CSstudy\lw11\toxonpf.txt";
+            void ex1()
+            {
+                Console.WriteLine("\n 1. Создание каталога.");
+
+                if (!File.Exists(fileName)) File.Create(fileName).Close();
+                else Console.WriteLine($"File already exists:");
+            }
+            ex1();
+
+            void ex2()
+            {
+                Console.WriteLine("\n 2. Создание списка задач");
+
+                if (File.Exists(fileName)) {
+                    while (true)
+                    {
+                        Console.Write("Введите задачу:");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "exit") break;
+                        File.AppendAllText(fileName, input + Environment.NewLine);
+                    }
+                }   
+                else Console.WriteLine("File does not exist");
+            }
+            ex2();
+        }
+    }
+}
