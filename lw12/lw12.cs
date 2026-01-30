19cf2bf8080e45f62daedf2a6a37d016b4d93203
toxon <toxonpf@gmail.com>
2026-01-30 11:19:29 +0500

    Добавлен проект lw12: чтение чисел из файла и среднее
    В решение добавлен новый проект lw12 с консольным приложением на C#. Программа читает числа из файла toxonpf.txt, вычисляет их среднее значение и выводит результат. Реализована обработка ошибок при работе с файлом. Добавлены необходимые файлы проекта и входной текстовый файл с числами.

---
+++ lw12/lw12.cs
@@ -0,0 +1,41 @@
+﻿namespace lw12
+{
+    public class lw12
+    {
+        public static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\toxon\source\repos\toxonpf\CSstudy\lw12\toxonpf.txt";
+            try
+            {
+                if (!File.Exists(fileLocation)) throw new FileNotFoundException("файл не найден");
+
+                string[] lines = File.ReadAllLines(fileLocation);
+
+                List<int> allNums = new List<int>();
+                foreach (string line in lines)
+                {
+                    int[] nums = line.Split(", ").Select(int.Parse).ToArray();
+                    allNums.AddRange(nums);
+                }
+
+                Console.WriteLine($"Среднее число: {allNums.Average()}");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Ошибка при чтении файла: {ex.Message}");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine($"Ошибка доступа к файлу: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+    }
+}
